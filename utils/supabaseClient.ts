3e48e7b7d2d9a2ce60c93895a631d1a47f36b806
jsiwek999 <julian.siwek@gmail.com>
2025-05-20 14:42:09 -0400

    chore: replace utils/supabaseClient.ts

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,3 +1,23 @@
+// supabaseClient.ts
+import { createBrowserClient } from '@supabase/ssr';
+
+let supabase: ReturnType<typeof createBrowserClient> | null = null;
+
+// only initialize in browser
+if (typeof window !== 'undefined') {
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const key = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
+
+  if (!url || !key) {
+    throw new Error(
+      "⚠️ Missing NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_KEY"
+    );
+  }
+
+  supabase = createBrowserClient(url, key);
+}
+
+export { supabase };
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
