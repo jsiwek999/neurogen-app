dc70923a6de7eb7beedd2162cf99e8c895541354
jsiwek999 <julian.siwek@gmail.com>
2025-09-01 14:15:19 -0400

    Fix import path for Navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 ﻿import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Navbar from "../components/Navbar";
 import { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
